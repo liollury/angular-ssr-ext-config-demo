d6dfdbc1f2fd58ac68b0a800d5fb4c1e03db609a
liollury <liollury@gmail.com>
2022-07-21 16:25:48 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -4,33 +4,37 @@ import { catchError, from, switchMap, throwError } from 'rxjs';
 import { AppModule } from './app/app.module';
 import { CONFIGURATION, MyConfiguration } from './my-configuration.interface';
 
-const configurationPath = '/config.json';
+// Full link to github because assets does not work on Stackblitz
+const configurationPath =
+  'https://raw.githubusercontent.com/liollury/angular-ssr-ext-config-demo/master/config/dev/config.json';
 
-from(fetch(configurationPath)).pipe(
-  switchMap((data) => from(data.json())),
-  catchError((err: any) => {
-    console.error(err);
-    return throwError(err)
-  })
-).subscribe((config: MyConfiguration) => {
-  if (config.production) {
-    enableProdMode();
-  }
+from(fetch(configurationPath))
+  .pipe(
+    switchMap((data) => from(data.json())),
+    catchError((err: any) => {
+      console.error(err);
+      return throwError(err);
+    })
+  )
+  .subscribe((config: MyConfiguration) => {
+    if (config.production) {
+      enableProdMode();
+    }
 
-  function bootstrap() {
-    platformBrowserDynamic([
-      {
-        provide: CONFIGURATION,
-        useValue: config
-      }
-    ]).bootstrapModule(AppModule)
-      .catch(err => console.error(err));
-  };
+    function bootstrap() {
+      platformBrowserDynamic([
+        {
+          provide: CONFIGURATION,
+          useValue: config,
+        },
+      ])
+        .bootstrapModule(AppModule)
+        .catch((err) => console.error(err));
+    }
 
-  if (document.readyState === 'complete') {
-    bootstrap();
-  } else {
-    document.addEventListener('DOMContentLoaded', bootstrap);
-  }
-});
-
+    if (document.readyState === 'complete') {
+      bootstrap();
+    } else {
+      document.addEventListener('DOMContentLoaded', bootstrap);
+    }
+  });
